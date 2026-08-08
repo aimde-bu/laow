aff4a0143b8a12ec152bb3f2adc1236bc2ebfa2c
about <3691507519@qq.com>
2026-08-08 21:45:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { WebSocket, createWebSocketStream } = require('ws');
 function def(env, val) { if (val) process.env[env] = process.env[env] || val; }
 
 def('uuid', 'd9b609fe-5b77-4100-9b46-bfc7888bcd9d');            // ←【必填】UUID,在线生成 https://www.uuidgenerator.net
-def('DOMAIN', '');          // ←【必填】你的域名,没有就填服务器IP
+def('DOMAIN', 'kalila-8d9ux7yrla.dcdeploy.cloud');          // ←【必填】你的域名,没有就填服务器IP
 
 def('vlpt', '20518');            // Vless-reality 端口
 def('vmpt', '');            // Vmess-ws 端口
